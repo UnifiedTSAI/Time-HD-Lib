6d694df17186ab7856d03fd97f279ba7b9fad335
Juntong Ni <85066551+LingFengGold@users.noreply.github.com>
2025-05-20 16:50:57 -0400

    Delete download_dataset.py

--- download_dataset.py
+++
@@ -1,8 +0,0 @@
-from datasets import load_dataset
-from huggingface_hub import login
-
-login("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")
-
-for data_name in ['m5', 'smart_meters_in_london', 'wikipedia_web_traffic', 'global_temp', 'global_wind', 'nrel_solar_power', 'sirs', 'atec', 'google_community_mobility', \
-                  'neurolib', 'rebound', 'sp500', 'china_air_quality', 'measles_england', 'largest_ca', 'largest_gba', 'largest_gla', 'wikipedia_web_traffic_2000', 'wikipedia_web_traffic_20000']:
-    dict_raw = load_dataset("lingfenggold/High_Dimensional_Time_Series", data_name, cache_dir="dataset")
